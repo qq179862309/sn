2cce746e4b39b6f061316695722038417760bc40
qq179862309 <179862309@qq.com>
2017-07-11 01:28:48 +0800

    2017年7月11日01:28:45

--- SN/hook/Hook.h
+++ SN/hook/Hook.h
@@ -2,31 +2,32 @@
 
 #include <map>
 
-using namespace std;
-
-typedef struct _tagHOOKINFO
+namespace SN
 {
-	DWORD	dwFuncAddress;
-	byte*	pNewCode;
-	int		nNewLength;
-	byte*	pOldCode;
-}HOOKINFO, *PHOOKINFO;
+	typedef struct _tagHOOKINFO
+	{
+		DWORD	dwFuncAddress;
+		byte*	pNewCode;
+		int		nNewLength;
+		byte*	pOldCode;
+	}HOOKINFO, *PHOOKINFO;
 
-typedef std::map<tstring, PHOOKINFO>	MAP_HOOKINFO;
-typedef MAP_HOOKINFO::iterator			ITER_HOOKINFO;
+	typedef std::map<tstring, PHOOKINFO>	MAP_HOOKINFO;
+	typedef MAP_HOOKINFO::iterator			ITER_HOOKINFO;
 
-class CHook
-{
-public:
-	CHook();
-	virtual ~CHook();
-public:
-	static	CHook*	getInstance();
-	static  int		hookWindowAPI(tstring strModule, char* funcName, DWORD dwFunction);
-public:
-	int				hookFunction(tstring strKey, DWORD dwFuncAddress, byte* lpNewCode, int nNewLength);
-	int				unHookFunction(tstring strKey);
-protected:
-	MAP_HOOKINFO	m_MapInfo;
-};
+	class SNAPI CHook
+	{
+	public:
+		CHook();
+		virtual ~CHook();
+	public:
+		static	CHook*	getInstance();
+		static  int		hookWindowAPI(tstring strModule, char* funcName, DWORD dwFunction);
+	public:
+		int				hookFunction(tstring strKey, DWORD dwFuncAddress, byte* lpNewCode, int nNewLength);
+		int				unHookFunction(tstring strKey);
+	protected:
+		MAP_HOOKINFO	m_MapInfo;
+	};
+}
 
